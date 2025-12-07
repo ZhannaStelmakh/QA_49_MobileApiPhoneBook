57bb64dbe3558244deb46f9d22bbe37d0e9f7cbf
ZhannaStelmakh <vertebrology.center@gmail.com>
2025-12-07 13:01:52 +0200

    create Appium Config

---
+++ src/test/java/tests_mobile/TestBase.java
@@ -0,0 +1,30 @@
+package tests_mobile;
+
+import io.appium.java_client.AppiumDriver;
+import manager.AppiumConfig;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+    public class TestBase {
+        protected static AppiumDriver driver;
+
+        @BeforeMethod
+        public void setup() {
+            driver = AppiumConfig.createAppiumDriver("pixel.properties");
+        }
+
+        @Test(enabled = false)
+        public void start(){
+
+        }
+
+        @AfterMethod(enabled = false)
+        public void tearDown() {
+            if (driver != null)
+                driver.quit();
+        }
+    }
+
